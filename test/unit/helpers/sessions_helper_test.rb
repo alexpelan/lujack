fd97a1ad5a076852f950f28793df2b7ae597e975
Alex Pelan <alexpelan@gmail.com>
2014-06-10 17:28:29 -0500

    Get test suite into working order (fix errors caused by migrations). Add unit tests for helpers

--- test/unit/helpers/sessions_helper_test.rb
+++ test/unit/helpers/sessions_helper_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class SessionsHelperTest < ActionView::TestCase
+	include SessionsHelper
+
+        test "initialize show try again doesnt modify false value" do
+                @show_try_again = false
+                initialize_show_try_again
+                assert_equal(@show_try_again, false)
+        end
+
+        test "initialize show try again sets nil to true" do
+                initialize_show_try_again
+                assert_equal(@show_try_again, true)
+        end
+
+        test "initialize show try again doesnt modify true value" do
+                @show_try_again = true
+                initialize_show_try_again
+                assert_equal(@show_try_again, true)
+	end
 end
